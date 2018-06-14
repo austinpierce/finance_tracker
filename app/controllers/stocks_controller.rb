dc8aae2559c6c78101a6141c01ede2bbff9d3a48
Austin Pierce <AustinPierce@gmail.com>
2018-06-13 23:12:11 -0400

    Handle null and invalid stock searches

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,10 +1,19 @@
 class StocksController < ApplicationController
  
   def search
-   @stock = Stock.new_from_lookup(params[:stock]) # referencing model. 
-    # where is :stock coming from. Stock model?
-    # render json: @stock
-    render 'users/my_portfolio'
-  end
-  
+    if params[:stock].present?
+      @stock = Stock.new_from_lookup(params[:stock]) # referencing model. 
+      # :stock is coming from text_field_tag in view
+      # render json: @stock
+      if @stock # if @stock exists it means it is not nil (check model)
+        render 'users/my_portfolio'
+      else
+        flash[:danger] = "You have entered an invalid stock"
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:danger] = "Please enter a stock"
+      redirect_to my_portfolio_path
+    end
+  end 
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,10 +2,13 @@ class Stock < ApplicationRecord
   
   def self.new_from_lookup(ticker_symbol) 
      # self level method (not working with object)
-    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-    new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price ) # new creates object
-    #name: , ticker:, last_price: are from DB columns. The . are from gem
-   
-  end
+    begin   
+      looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+      new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price ) # new creates object
+    rescue Exception => e # if user enters symbol that doesn't exist
+      return nil
+    end
+      #name: , ticker:, last_price: are from DB columns. The . are from gem   
+    end
   
 end
