3d808668990fc2b154a375c9d951265fb50d6f5c
Austin Pierce <AustinPierce@gmail.com>
2018-06-14 21:55:31 -0400

    Refactor code and error handeling

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,21 +1,17 @@
 class StocksController < ApplicationController
  
-  def search
-    if params[:stock].present?
-      @stock = Stock.new_from_lookup(params[:stock]) # referencing model. 
-      # :stock is coming from text_field_tag in view
-      # render json: @stock
-      if @stock # if @stock exists it means it is not nil (check model)
-        respond_to do |format| # for ajax
-          format.js { render partial: 'users/resultx' }
-        end
-      else
-        flash[:danger] = "You have entered an invalid stock"
-        redirect_to my_portfolio_path
-      end
-    else
-      flash[:danger] = "Please enter a stock"
-      redirect_to my_portfolio_path
+def search
+  if params[:stock].blank?
+    flash.now[:danger] = "You have entered an empty search"
+  else
+    @stock = Stock.new_from_lookup(params[:stock]) # referencing model.
+    # :stock is coming from text_field_tag in view
+    flash.now[:danger] = "You have entered an invalid stock" unless @stock
+    # unless @stock exists it means it is not nil (check model)
+  end
+    respond_to do |format| # for ajax // moved this here for DRY
+      format.js { render partial: 'users/resultx' } 
     end
   end 
+
 end
